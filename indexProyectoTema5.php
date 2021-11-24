1ccf3b5f7662cd50d7995ba7674e0180e4e4cf26
oscar <oscar.llapar@educa.jcyl.es>
2021-11-24 13:41:52 +0100

    24/11/2021 Commit inicial

---
+++ indexProyectoTema5.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>OLP-DWES - Proyecto Tema 5</title>
+        <link href="webroot/css/estilos.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <header>
+            <h1>Desarrollo Web en Entorno Servidor</h1>
+            <h4>Tema 5</h4>
+        </header>
+        <main>
+            <div class="ejercicio">
+                <p></p>
+                <a><div class="boton"><img src="webroot/img/iconoPlay.png"></div></a>
+                <a><div class="boton"><img src="webroot/img/iconoMostrar.png"></div></a>
+            </div>
+            <div class="ejercicio">
+                <p></p>
+                <a><div class="boton"><img src="webroot/img/iconoPlay.png"></div></a>
+                <a><div class="boton"><img src="webroot/img/iconoMostrar.png"></div></a>
+            </div>
+            <div class="ejercicio">
+                <p></p>
+                <a><div class="boton"><img src="webroot/img/iconoPlay.png"></div></a>
+                <a><div class="boton"><img src="webroot/img/iconoMostrar.png"></div></a>
+            </div>
+        </main>
+        <footer>
+            <p>
+                Óscar Llamas Parra &nbsp;
+                <a href="https://github.com/OscarLlaPar/" target="__blank"><img src="webroot/img/github.png" alt="Github"></img></a>
+            </p>
+            <p>
+                DAW 2
+            </p>
+            <p>
+                IES Los Sauces, Benavente 2021-2022
+            </p>
+            <div class="cuadro" id="abajo"></div>
+        </footer>
+    </body>
+</html>
---
+++ webroot/img/indexProyectoTema5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>OLP-DWES - Proyecto Tema 5</title>
+        <link href="webroot/css/estilos.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <header>
+            <h1>Desarrollo Web en Entorno Servidor</h1>
+            <h4>Tema 5</h4>
+        </header>
+        <main>
+            <div class="ejercicio">
+                <p></p>
+                <a><div class="boton"><img src="webroot/img/"></div></a>
+                <a><div class="boton"></div></a>
+            </div>
+        </main>
+        <footer></footer>
+    </body>
+</html>
