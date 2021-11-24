ac72028fe64429e8c8da5301e5cda87d275b6fb9
Oscar <oscar.llapar@educa.jcyl.es>
2021-11-25 00:29:56 +0100

    25/11/2021: Creación del index

--- indexProyectoTema5.php
+++ indexProyectoTema5.php
@@ -9,20 +9,25 @@
         <header>
             <h1>Desarrollo Web en Entorno Servidor</h1>
             <h4>Tema 5</h4>
+            <a href="../ProyectoDWES/indexProyectoDWES.php"><div class="cuadro" id="arriba">&#60;</div></a>
         </header>
         <main>
             <div class="ejercicio">
-                <p></p>
+                <div class="num">0</div>
+                <p>Mostrar el contenido de las variables superglobales y phpinfo()</p>
                 <a><div class="boton"><img src="webroot/img/iconoPlay.png"></div></a>
                 <a><div class="boton"><img src="webroot/img/iconoMostrar.png"></div></a>
             </div>
             <div class="ejercicio">
-                <p></p>
+                <div class="num">1</div>
+                <p>Desarrollo de un control de acceso con identificación del usuario basado en la función header()</p>
                 <a><div class="boton"><img src="webroot/img/iconoPlay.png"></div></a>
                 <a><div class="boton"><img src="webroot/img/iconoMostrar.png"></div></a>
             </div>
             <div class="ejercicio">
-                <p></p>
+                <div class="num">2</div>
+                <p>Desarrollo de un control de acceso con identificación del usuario basado en la función header() y
+en el uso       de una tabla “Usuario” de la base de datos. (PDO)</p>
                 <a><div class="boton"><img src="webroot/img/iconoPlay.png"></div></a>
                 <a><div class="boton"><img src="webroot/img/iconoMostrar.png"></div></a>
             </div>
